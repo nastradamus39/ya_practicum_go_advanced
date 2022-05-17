ecd0035504a53e27afa4f67dfa1fb2a4a237656b
nastradamus39 <a.v.krizhanovsky@k50dev.ru>
2022-05-18 01:01:46 +0300

    fix test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,32 +2,31 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
-
+	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	handlers "github.com/nastradamus39/ya_practicum_go_advanced/internal/handlers"
+	"log"
+	"net/http"
 )
+
+type Config struct {
+	BaseURL       string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
+}
 
 func main() {
 	r := router()
 
-	// export SERVER_ADDRESS='127.0.0.1'
-	// export BASE_URL='127.0.0.1'
-	//os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
-	//os.Setenv("BASE_URL", "http://127.0.0.1:8080")
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//os.Setenv("SERVER_HOST", "127.0.0.1")
-	//os.Setenv("SERVER_PORT", "8080")
+	fmt.Println(fmt.Printf("Starting server on %s", cfg.ServerAddress))
 
-	serverAddr := fmt.Sprintf("%s:%s", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))
-
-	os.Setenv("BASE_URL", fmt.Sprintf("http://%s", serverAddr))
-
-	fmt.Println(fmt.Printf("Starting server on %s", serverAddr))
-
-	http.ListenAndServe(serverAddr, r)
+	http.ListenAndServe(cfg.ServerAddress, r)
 }
 
 func router() (r *chi.Mux) {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/go-chi/chi/v5 v5.0.7
 
 require (
+	github.com/caarlos0/env/v6 v6.9.2 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
