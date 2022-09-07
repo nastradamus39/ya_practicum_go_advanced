0f95a05a9d3eeb6abf4488a228f44178984d3144
nastradamus39 <a.v.krizhanovsky@k50dev.ru>
2022-09-07 21:42:25 +0300

    https для сервера

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"flag"
 	"log"
+	"math/big"
+	"net"
 	"net/http"
 	"net/http/pprof"
 	"os"
+	"time"
 
-	"github.com/caarlos0/env/v6"
-	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/nastradamus39/ya_practicum_go_advanced/internal/app"
 	"github.com/nastradamus39/ya_practicum_go_advanced/internal/handlers"
@@ -46,6 +53,53 @@ func main() {
 	log.Printf("Starting server on %s", app.Cfg.ServerAddress)
 	log.Println(app.Cfg)
 
+	// создаём шаблон сертификата
+	cert := &x509.Certificate{
+		// указываем уникальный номер сертификата
+		SerialNumber: big.NewInt(1658),
+		// заполняем базовую информацию о владельце сертификата
+		Subject: pkix.Name{
+			Organization: []string{"Yandex.Praktikum"},
+			Country:      []string{"RU"},
+		},
+		// разрешаем использование сертификата для 127.0.0.1 и ::1
+		IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		// сертификат верен, начиная со времени создания
+		NotBefore: time.Now(),
+		// время жизни сертификата — 10 лет
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		// устанавливаем использование ключа для цифровой подписи,
+		// а также клиентской и серверной авторизации
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// создаём сертификат x.509
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// кодируем сертификат и ключ в формате PEM, который
+	// используется для хранения и обмена криптографическими ключами
+	var certPEM bytes.Buffer
+	pem.Encode(&certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+
+	var privateKeyPEM bytes.Buffer
+	pem.Encode(&privateKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
+
 	// инициируем хранилище
 	err = storage.New(&app.Cfg)
 	if err != nil {
@@ -54,7 +108,7 @@ func main() {
 	}
 
 	// запускаем сервер
-	err = http.ListenAndServe(app.Cfg.ServerAddress, r)
+	err = http.ListenAndServeTLS(app.Cfg.ServerAddress, string(certBytes), string(privateKey), r)
 	if err != nil {
 		log.Printf("Не удалось запустить сервер. %s", err)
 		return
